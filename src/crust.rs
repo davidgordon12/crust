6e2f36f3a06d34c1be2de3bc60499689576e42b7
tehtko <moderndsnsam@gmail.com>
2023-11-07 19:15:00 -0500

    Finish implementing Crust

--- src/crust.rs
+++ src/crust.rs
@@ -1,16 +1,35 @@
-struct Request {
+use std::str::FromStr;
+
+use reqwest::{Method, Url, blocking::{self}};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CrustRequest {
     method: String,
     uri: String,
-    headers: Vec<String>,
 }
 
-struct Response {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CrustResponse {
     status: u8,
     body: String,
 }
 
-impl Request {
-    fn execute_request(&self) -> Response {
+impl CrustRequest {
+    pub fn new(method: String, uri: String) -> Self {
+        CrustRequest {
+            method: method,
+            uri: uri,
+        }
+    }
+
+    pub fn execute_request(&self) -> blocking::Response {
+        let req = reqwest::blocking::Request::new(
+            Method::from_str(&self.method.as_str()).unwrap(),
+            Url::from_str(self.uri.as_str()).unwrap()
+        );
         
+        let client = blocking::Client::new();
+        blocking::Client::execute(&client, req).unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use std::env;
 
+mod crust;
+use crust::CrustRequest;
+
 fn main() {
-    if env::args().len() < 3 {
-        print!("Too few arguments!");
+    if env::args().len() != 3 {
+        println!("Invalid arguments!");
+        println!("USAGE: crust <method> <url>");
+        return;
     }
+
+    let args: Vec<_> = env::args().collect();
+
+    let req: CrustRequest = CrustRequest::new(
+        args[1].to_string(), 
+        args[2].to_string()
+    );
+
+    let res = req.execute_request();
+
+    println!("{:#?}", res.status());
+    println!("{:#?}", res.text().unwrap());
 }
